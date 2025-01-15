e92d1554088ec3a37dbf701f4641185d19d762cb
Ali George <george7ali@gmail.com>
2025-01-15 11:48:54 +0100

    import the Entry component and create a function call noteEntry

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import Entry from "./Entry";
+
+function noteEntry(note){
+  return (
+    <Entry 
+      key={note.id}
+      title={note.title}
+      content={note.content}
+    />
+  )
+}
 
 function Note() {
   return (
     <div className="note">
-      <h1>This is the note title</h1>
-      <p>This is the note content</p>
+      
     </div>
   );
 }
